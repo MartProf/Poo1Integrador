053f8e9630476cdddf2569f10b0ae7b58ebd60d2
Federico Ramos <ramos12fede@gmail.com>
2025-06-30 10:43:37 -0300

    Subiendo las creaciones de las clases/interfaces/enums del modelo

--- demo/src/main/java/com/example/modelo/Concierto.java
+++ demo/src/main/java/com/example/modelo/Concierto.java
@@ -3,7 +3,6 @@ package com.example.modelo;
 import java.util.List;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,7 +16,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Concierto extends Evento {
 
-    
+
     private List <Persona> artistas;
     private boolean entradaGratuita;
 
